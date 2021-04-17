427e676616875caff9785117539bb5380677c8ba
filipe11402 <filipe11402@outlook.pt>
2021-04-17 19:49:01 +0100

    Changed request link

--- app.py
+++ app.py
@@ -1,37 +1,32 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
-
-# getting company symbol
+# getting the company
 def get_symbol(cmp_name):
-
     try:
-        url = ('https://finnhub.io/api/v1/search?q={}&token=c1phgr2ad3id1hoq14pg').format(cmp_name)
+        url = "https://finance.yahoo.com/quote/MSFT?p=MSFT&.tsrc=fin-srch" # use placeholders where MSFT is, to fetch price from yahoo finance
 
         response = requests.get(url)
 
-    except requests.HTTPError as error:
-        print("there was an error while trying to get company symbol")
+        soup = bs(response.content, 'html.parser')
+        print(soup.div['D(ib) Mend(20px)'])
+    except NameError as error:
+        print("Error while trying to get data")
 
 
-    # returning symbol for func get_price
-    return response.json()['result'][0]['symbol']
-
-
-def get_price(company):
+"""
+def get_price(cmp_symbol):
     try:
+        url = ("https://eodhistoricaldata.com/api/eod/{}?api_token=60754dbc9be494.79866025&fmt=json").format(cmp_symbol)
 
-        cmp_symbol = get_symbol(company)
-        print(cmp_symbol)
+        response = requests.get(url).json()
 
-        """
-        url = ("https://finnhub.io/api/v1/quote?symbol={}&token=c1phgr2ad3id1hoq14pg").format(cmp_symbol)
-
-        response = requests.get(url)
-
-        print(response)"""
+        print(response)
     except requests.HTTPError as error:
         print("Error while trying to get the price")
     
 
 
-get_price("microsoft")
+get_price("MSFT")"""
+
+get_symbol("Microsoft")
